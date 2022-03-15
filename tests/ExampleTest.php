21f3950aaebdbc89423dcac96800201e4315f95e
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2022-03-15 20:44:15 +0800

    refactor: refactor
    Signed-off-by: Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -2,6 +2,21 @@
 
 declare(strict_types=1);
 
-test('example', function () {
-    expect(true)->toBeTrue();
-});
+namespace Tests;
+
+use XNXK\LaravelEsign\Esign;
+use XNXK\LaravelEsign\EsignRefactor;
+
+class ExampleTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+       print(
+           (new EsignRefactor('token', 'token'))->account()->queryPersonalAccountByAccountId('test'));
+    }
+}
