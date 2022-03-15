bcc5a80fa09301487c5eacb114eb5ca924ab1230
YGNice <yg@lianhe.art>
2022-03-15 16:59:11 +0800

    refactor(config): app key to secret

--- src/config.php
+++ src/config.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 return [
     'debug' => env('DEBUG', false),
     'appId' => env('ESIGN_APPID', 'your-app-id'),                            // APP ID
-    'appKey' => env('ESIGN_APPKEY', 'your-app-key'),                          // APP KEY
+    'secret' => env('ESIGN_SECRET', 'your-app-secret'),                          // APP SECRET
     'server' => env('ESIGN_SERVER', 'https://smlopenapi.esign.cn'),           // esign api v2 url.
     'notify_url' => env('ESIGN_NOTIFY_URL', 'XXXXXX/api/esign/callback'),         // callback url
     // Orgs
