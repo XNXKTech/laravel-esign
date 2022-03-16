65995df644cdbb76941743b91b08c3bd8e749e7b
Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>
2022-03-16 11:39:15 +0800

    test: account endpoints setup
    Signed-off-by: Teakowa Gatanothor O'deorain <hub+git@teakowa.dev>

---
+++ tests/Endpoints/AccountsTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Endpoints;
+
+use Tests\TestCase;
+use XNXK\LaravelEsign\Esign;
+
+class AccountsTest extends TestCase
+{
+    public function testGetUserID()
+    {
+        (new Esign(env('ESIGN_APPID'), env('ESIGN_SECRET')))->account()->queryPersonalAccountByThirdId('testid');
+    }
+}
